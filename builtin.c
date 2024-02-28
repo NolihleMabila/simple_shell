8c5542b9253340234961f584dd42794bbafc9642
nolim <nolimcode@gmail.com>
2024-02-28 15:19:24 -0800

    Simple shell project

--- builtin.c
+++ builtin.c
@@ -33,48 +33,40 @@ int _myexit(info_t *info)
  * @info: Structure
  *  Return: Always 0
  */
+
 int _mycd(info_t *info)
 {
-	char *str, *dir, buffer[1024];
+	char *s, *dir, buffer[1024];
 	int chdir_ret;
 
-	str = getcwd(buffer, 1024);
-	if (!str)
-	{
-		_puts("TODO: >>getcwd failure emsg here.<<\n");
-	}
-
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
 		dir = _getenv(info, "HOME=");
 		if (!dir)
-		{
-			puts("Error: HOME not set\n");
-		return (1);
-		}
-		chdir_ret = chdir(dir);
+			chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
-		dir = _getenv(info, "OLDPWD=");
-		if (!dir)
+		if (!_getenv(info, "OLDPWD="))
 		{
-			_puts("Error: OLDPWD not set\n");
+			_puts(s);
+			_putchar('\n');
 			return (1);
 		}
-		_puts(dir);
-		_putchar('\n');
-		chdir_ret = chdir(dir);
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
-
 	else
 		chdir_ret = chdir(info->argv[1]);
-
 	if (chdir_ret == -1)
 	{
-		print_error(info, "cannot command to ");
+		print_error(info, "can't cd to ");
 		_eputs(info->argv[1]), _eputchar('\n');
-		return (1);
 	}
 	else
 	{
@@ -83,6 +75,8 @@ int _mycd(info_t *info)
 	}
 	return (0);
 }
+
+
 /**
  * _myhelp - prints help information
  * @info: Structure containing command information
